3a5b9af5bcd57ae86b1156b6647e2ba522ed6654
Leandro <leandro.gocosta@gmail.com>
2021-04-06 17:23:42 -0300

    primeiro commit

---
+++ src/main/java/com/inuvens/api/SpringServiceApplication.java
@@ -0,0 +1,15 @@
+package com.inuvens.api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServiceApplication.class, args);
+	}
+
+}
